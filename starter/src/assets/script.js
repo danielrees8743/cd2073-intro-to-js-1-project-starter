678f05cff3140e6f3038fd143a17315bc4954111
Dan Rees <danrees8743@gmail.com>
2022-08-03 00:31:19 +0100

    Added another function

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -13,21 +13,21 @@ const products = [
     name: 'Cherry',
     price: 0.75,
     quantity: 0,
-    productId: 001,
+    productId: 101,
     image: './images/cherry.jpg',
   },
   {
     name: 'Orange',
     price: 1.0,
     quantity: 0,
-    productId: 002,
+    productId: 102,
     image: './images/orange.jpg',
   },
   {
     name: 'Strawberry',
     price: 1.1,
     quantity: 0,
-    productId: 003,
+    productId: 103,
     image: './images/strawberry.jpg',
   },
 ];
@@ -39,42 +39,62 @@ const products = [
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
-
+const cart = [];
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(productId) {
+  // if (!productId === cart) {
+  products.forEach((product) => {
+    if (productId === product.productId) {
+      cart.push(product);
+      product.quantity++;
+      return products.quantity;
+    }
+  });
+  // }
+}
+addProductToCart(101);
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-
+function increaseQuantity(productId) {
+  cart.find((product) => product === productId);
+  quantity++;
+}
+increaseQuantity(101);
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
+function decreaseQuantity(productId) {
+  cart.find((product) => product === productId);
+  quantity--;
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
-
+function removeProductFromCart() {}
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
 
 /* Create a function called emptyCart that empties the products from the cart */
-
+function emptyCart() {}
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
-
+function pay() {}
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
 /* The following is for running unit tests.
